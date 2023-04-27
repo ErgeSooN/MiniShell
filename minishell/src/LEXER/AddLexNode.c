8debd0dc3bcfd78e1401a8413f376c1f4274e366
ErgeSooN <65658818+ErgeSooN@users.noreply.github.com>
2023-04-27 22:53:01 +0000

    new_commits

--- minishell/src/LEXER/AddLexNode.c
+++ minishell/src/LEXER/AddLexNode.c
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   AddLexNode.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbolat <bbolat@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ayaman <ayaman@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 13:30:16 by bbolat            #+#    #+#             */
-/*   Updated: 2023/01/19 13:30:16 by bbolat           ###   ########.fr       */
+/*   Updated: 2023/04/27 22:18:12 by ayaman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../Include/minishell.h"
+
+/*
+  Bu kod eğer cmd_table değişkeni yoksa oluşturur daha sonra bağlı listenin sonuna yeni 
+  yeni bir düğüm ekler bunu yapmamızın amacı kullanıcı bir komut satırına yeni bir argüman 
+  eklediğinde veya yeni bir dosya yolunu belirttiğinde, bu fonksiyonun kullanılabilmesidir.
+*/
 
 t_lexlist	*add_new_lex_node(t_lexlist **cmd_table)
 {
--- minishell/src/UTILITS/STR/TrimSpace.c
+++ minishell/src/UTILITS/STR/TrimSpace.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   TrimSpace.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbolat <bbolat@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ayaman <ayaman@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 13:37:05 by bbolat            #+#    #+#             */
-/*   Updated: 2023/01/19 13:37:05 by bbolat           ###   ########.fr       */
+/*   Updated: 2023/04/27 19:42:12 by ayaman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../Include/minishell.h"
+
+/*Eğer girilen metinde space var ise bu boşlukları ortadan kaldırır*/
 
 size_t	trim_spaces(char **cmd_line)
 {
