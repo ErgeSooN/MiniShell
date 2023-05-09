ff68d048953b9701a30548e25f4d2e9493e2f89c
Hilal Nur Yolasığmaz <99798597+hilalny@users.noreply.github.com>
2023-05-09 22:57:12 +0000

    hny9

--- minishell/src/signals.c
+++ minishell/src/signals.c
@@ -12,6 +12,13 @@
 
 #include "../Include/minishell.h"
 
+/*
+	programın kullanıcı sinyali (CTRL + C gibi) alması durumunda çağrılır. Eğer g_core.cmd
+	değeri henüz atanmamışsa (yani henüz bir komut çalıştırılmamışsa), programın kaynaklarını
+	(free_core() fonksiyonunu çağırarak) serbest bırakır ve ardından programı exit(131) çağrısı
+	ile sonlandırır.
+*/
+
 void	exit_signal_check(void)
 {
 	if (!g_core.cmd)
@@ -21,6 +28,25 @@ void	exit_signal_check(void)
 		exit(131);
 	}
 }
+
+/*
+	Sinyal yakalama işlemini yapar ve kullanıcının sinyal aldığında programı güvenli bir
+	şekilde sonlandırmasını sağlar.
+	Sinyal numarasının 2 olması kullanıcının CTRL+C tuşlarına bastığı anlamına gelir.
+
+	g_core.exit_status değişkeni, 130 değeri ile atanır. Bu değer, programın sinyal nedeniyle 
+	sonlandırıldığını belirtmek için kullanılan bir değerdir.
+
+	"\n" karakteri stdout'a yazdırılır, bu işlem kullanıcının konsoluna yeni satır ekler.
+
+	Readline kitaplığı, rl_on_new_line fonksiyonu çağrısı ile yeni bir satırın başına dönülür.
+
+	Konsol girdisi rl_replace_line("", 0) ile silinir ve Readline kitaplığına gösterilir.
+	
+	rl_redisplay() fonksiyonu, Readline kütüphanesi tarafından yeniden çizilmesi gereken bölgeyi
+	yeniden çizer. Bu işlem, terminal ekranını doğru bir şekilde günceller.
+
+*/
 
 void	signals(int sig)
 {
